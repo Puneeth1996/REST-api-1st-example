e6662846e01db4dd2caf9f40e40ed3515307913b
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2019-05-08 08:39:23 +0530

    Actual solution Wedeeye
    Pymongo - Python Flask with Mongo Db Backend for rest api interaction

--- wedeeeye - backend - rest apo python - flask - mongo - pymongo.py
+++ wedeeeye - backend - rest apo python - flask - mongo - pymongo.py
@@ -1,5 +1,3 @@
-import pymongo
-# the above module is being put just for understanding from the w3chools page basics
 from flask import Flask,jsonify,request
 from flask_pymongo import PyMongo
 
@@ -12,93 +10,89 @@ mongo = PyMongo(app)
 print("connected i guess")
 user = mongo.db.users
 
-
-
-# make sure this below route was used just on start test process
-# @app.route('/add')
-# def add():
-# 	user.insert({'name':'puneeth'})
-# 	return "added user!"
-
-@app.route('/add_plans')
+#This below function create the home route that shows out all the database plans existing.
+@app.route('/', methods=['GET'])
 def all_plans():
-	user.insert({"id" : "1", "name" : "Plan1", "price" : { "amount" : 10, "currency" : "kwd" }, "features" : [ { "id" : "1.1", "name" : "Calls", "limit" : { "amount" : 100, "unit" : "Minutes", "Balance" : 100, "extraCharge" : { "amount" : "0.100", "currency" : "kwd", "unit" : "minute" } } }, { "id" : "1.2", "name" : "SMS", "limit" : { "amount" : 250, "unit" : "Messages", "Balance" : 250, "extraCharge" : { "amount" : "0.025", "currency" : "kwd", "unit" : "message" } } } ] })
-	user.insert({"id" : "2", "name" : "Plan2", "price" : { "amount" : 25, "currency" : "kwd" }, "features" : [ { "id" : "2.1", "name" : "Calls", "limit" : { "amount" : 500, "unit" : "Minutes", "Balance" : 500, "extraCharge" : { "amount" : "0.200", "currency" : "kwd", "unit" : "minute" } } }, { "id" : "2.2", "name" : "SMS", "limit" : { "amount" : 500, "unit" : "Messages", "Balance" : 500, "extraCharge" : { "amount" : "0.05", "currency" : "kwd", "unit" : "message" } } } ] })
-	user.insert({"id" : "3", "name" : "Plan3", "price" : { "amount" : 25, "currency" : "kwd" }, "features" : [ { "id" : "2.1", "name" : "Calls", "limit" : { "amount" : 500, "unit" : "Minutes", "Balance" : 500, "extraCharge" : { "amount" : "0.200", "currency" : "kwd", "unit" : "minute" } } }, { "id" : "2.2", "name" : "SMS", "limit" : { "amount" : 500, "unit" : "Messages", "Balance" : 500, "extraCharge" : { "amount" : "0.05", "currency" : "kwd", "unit" : "message" } } } ] })
-	return "added 3 intial plans!"
-
-
-@app.route('/plan', methods=['GET'])
-def base_plans():
 	output = []
-	output1 = []
 	for val in user.find():
-		print(val)
-		output.append({'Plan': val['name']})
-		print(output)
-		for n in val["price"]:
-			# print(n)
-			# print(val["price"][n])
-			output1.append({'Amount':val["price"][n]})
-			print(output1)
-		output.append(output1)		
-	return jsonify({'result' : output})
+		output.append({'its id': val['id']})
+		output.append({'Plan-Name': val['name']})
+		for n in val['price']:
+			if(n == 'amount'):
+				output.append({'Amount':val['price'][n]})
+			
+			elif(n == 'currency'):
+				output.append({'Currency':val['price'][n]})
+		
+		for m in val['features']:
+			output.append({'Feature': m['name']})
+			for lim in m['limit']:				
+				if(lim == 'unit'):
+					output.append({'Unit':m['limit']['unit']})					
+				elif(lim == 'Balance'):
+					bal = (m['limit']['Balance'])
+					output.append({'Balance': bal})
+					
+	return jsonify({'All Plans Currently Existing' : output})	
 
 
-@app.route('/plan/<path:filename>')
-def particular_plan(filename):
-	search = filename
-	print(search)
+#this below route helps in printing out the current existing plans in database
+#for this update method is made only to accept the id and print out a new updated id for the plan 
+@app.route('/<path:a_plan>')
+def new_plan(a_plan):
 	out = []
-	out1 = []
-	s = user.find_one({'name' : search})
-	print(s)
-	if s:
-		out = {'Plan' : s['name']}
-		print(out)
-		for n in s["price"]:
-			# print(s["price"][n])
-			out1.append({'Amount':s["price"][n]})
-		print(out1)
-	return jsonify({'result' : [out,out1]})	
+	the_plan = user.find_one({'name': a_plan})
+	if (the_plan):
+		for n in the_plan['price']:
+			if(n == 'amount'):
+				out.append({'Amount':the_plan['price'][n]})
+		
+			elif(n == 'currency'):
+				out.append({'Currency':the_plan['price'][n]})
+				
+		for m in the_plan['features']:
+			out.append({'Feature': m['name']})
+			for lim in m['limit']:				
+				if(lim == 'unit'):
+					out.append({'Unit':m['limit']['unit']})					
+				elif(lim == 'Balance'):
+					bal = (m['limit']['Balance'])
+					out.append({'Balance': bal})	
+	else:
+		out.append("the plan entered"+a_plan+" is incorect")
+					
+	return jsonify({'The '+a_plan+' has ' : out})
+
+#this below route hepls in creating the add a plan to the connected database
+@app.route('/add_a_plan', methods=['POST'])
+def add_star():
+	the_new_plan = request.json
+	print(the_new_plan)
+	new_plan = user.find_one({'name': the_new_plan['name']})
+	if(new_plan):
+		return("the entered plan is existing in database"+the_new_plan['name'])
+	else:
+		user.insert(the_new_plan)
+		return ("Added : "+ the_new_plan['name'])
+	
 
 
-
-@app.route('/add_one_plan', methods=['POST'])
-def add_one():
-	star = user
-	print(star)
-	print(70*'=')
-	name = request.json['name']
-	print(name)
-	star_id = star.insert({'name': name})
-	new_star = star.find_one({'_id': star_id })
-	output = {'name' : new_star['name']}
-	return jsonify({'result' : output})
-
-
+#this below route is made for update
+#user enter the plan_id in path and passes the new json value to be updated
+@app.route('/<path:plan_id>/update', methods=['POST'])
+def update_a_plan(plan_id):
+	output = []
+	print(plan_id)
+	the_plan_updating = user.find_one({'id': plan_id})
+	print(the_plan_updating)
+	print({'id': plan_id})
+	if(the_plan_updating):
+		output.append({'the request':(request.json)})
+		update_val = request.json
+		user.update_many({'id': plan_id}, {"$set":request.json})
+		return ("the plan "+plan_id+" was updated.")
+	else:
+		return ("the "+plan_id+" is not existing in the database...")
+	
 if __name__ == '__main__':
 	app.run(debug=True)
-
-
-
-
-
-# print(50*'=')
-# myclient = pymongo.MongoClient("mongodb+srv://test:test@wideeye-3ku58.mongodb.net/test?retryWrites=true")
-# print(myclient)
-# dblist = myclient.list_database_names()
-# print(dblist)
-# mydb = myclient["wedeeye"]
-# collist = mydb.list_collection_names()
-# print(collist)
-# mycol = mydb["plans"]
-# x = mycol.find_one()
-# print(x)
-
-# @app.route('/', methods=['GET'])
-# def entry_plans():
-# 	return jsonify('All plans')
-
-
-
